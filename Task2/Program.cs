01d43e091f85a91b77a0160118924c0608be54be
Stitch710 <dima_pichugin_2016@mail.ru>
2022-03-07 13:08:22 +0300

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите кол-во линий");
+            if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+                for (string i = "*"; i.Length <= n; i += "*")
+                    Console.WriteLine(i);
+            else
+                Console.WriteLine("введите коректное значение");
+        }
+    }
+}
